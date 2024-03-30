49a9097ebdbff9544fd7dcf44f68da40fb0db9cf
pol <140607340+polmt@users.noreply.github.com>
2024-03-30 19:27:59 +0200

    Update q1.py

--- digital_transceivers/ex6/q1.py
+++ digital_transceivers/ex6/q1.py
@@ -1,11 +1,8 @@
 import numpy as np
 
-# Define the 4-QAM symbols
 qam_symbols = np.array([1 + 1j, 1 - 1j, -1 + 1j, -1 - 1j])
 
-# Generate a random 4x1 vector of 4-QAM symbols
 Tx = np.random.choice(qam_symbols, size=(4, 1))
 
-# Print the resulting vector
 print("Tx:")
 print(Tx)
